3242bef2bea56747ed2b82136a5eda683184efb7
juandiegows <mejiamaestrejuandiego@gmail.com>
2022-08-08 17:27:35 -0500

    Pantalla completa

--- app/src/main/java/com/example/modulo7wsc2017mobile/MainActivity.kt
+++ app/src/main/java/com/example/modulo7wsc2017mobile/MainActivity.kt
@@ -1,11 +1,35 @@
 package com.example.modulo7wsc2017mobile
 
-import androidx.appcompat.app.AppCompatActivity
+import android.os.Build
 import android.os.Bundle
+import android.view.View
+import android.view.View.SYSTEM_UI_FLAG_IMMERSIVE
+import android.view.Window
+import android.view.WindowInsets
+import android.view.WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+       PantallaCompleta()
+    }
+
+
+    private fun PantallaCompleta() {
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        val controller = WindowInsetsControllerCompat(window, window.decorView)
+        controller.hide(WindowInsetsCompat.Type.systemBars())
+        controller.systemBarsBehavior =
+            WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+
+
     }
 }
